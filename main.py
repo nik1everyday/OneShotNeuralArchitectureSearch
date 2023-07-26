528a95511347bf04df9e05c897484dac3eb6e03a
Nikita Borisov <nik-everyday@yandex.ru>
2023-07-26 13:17:43 +0300

    Added `main.py`

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+
+from model.train_test import train_test
+from model.utils import generate_plot_accuracies, generate_accuracy_table
+from model.model import SuperNet
+
+
+def main(epochs):
+    print('Training and testing oneshot subnets...')
+    one_shot_nas_accuracies = train_test(SuperNet(), epochs)
+
+    print('Training and testing standalone subnets...')
+    standalone_accuracies = []
+    for subnet in [(x, y) for x in range(3) for y in range(3)]:
+        supernet = SuperNet()
+        standalone_accuracies.append(train_test(supernet, epochs, subnet=subnet))
+
+    print('Generating oneshot subnets accuracies plot...')
+    generate_plot_accuracies('oneshot', one_shot_nas_accuracies,
+                             'One Shot Training Progress', 'pics/one-shot-subnets.png')
+    print('Generating standalone subnets accuracies plot...')
+    generate_plot_accuracies('standalone', standalone_accuracies,
+                             'Standalone Training Progress', 'pics/standalone-subnets.png')
+
+    print('Generating oneshot-vs-standalone subnets top-1 accuracies table...')
+    generate_accuracy_table(one_shot_nas_accuracies,
+                            standalone_accuracies,
+                            'pics/oneshot_vs_standalone_table.png')
+
+
+if __name__ == '__main__':
+    epochs = int(sys.argv[1])
+    main(epochs)
